d08551011c4ee197d87fd57dba693b25745f930c
wangcsu <wk.qwerty17@gmail.com>
2020-04-04 23:42:55 +0800

    array and func

---
+++ arrays.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func printArray(arr *[5]int) {
+	arr[0] = 100
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func main() {
+	var arr1 [5]int
+	arr2 := [3]int{1, 3, 5}
+	arr3 := [...]int{2, 4, 6, 8, 10}
+	var grid [4][5]int
+
+	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(grid)
+
+	for _, v := range arr3 {
+		fmt.Println(v)
+	}
+
+	printArray(&arr1)
+	printArray(&arr3)
+	// printArray(arr2)
+
+	fmt.Println(arr1, arr3)
+}
